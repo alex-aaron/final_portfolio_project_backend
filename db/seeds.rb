8c220c5ab5022ca3398a7c6285737ae5835acf11
Alex <alexfrancisaaron@gmail.com>
2021-05-06 09:36:09 -0500

    configured cors initializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post2 = Post.create(title: "Ozark", content: "On season 2 right now. So good.")
+post3 = Post.create(title: "30 Rock", content: "Season 5. Queen of Jordan is hilarious.")
+post4 = Post.create(title: "Unwell", content: "Netflix show about wellness industry. Meh.")
+
+comment2 = Comment.create(post_id: 2, content: "Such a good show.")
+comment3 = Comment.create(post_id: 3, content: "My way till pay day.")
+comment4 = Comment.create(post_id: 4, content: "Agreed. I watched the episode on essential oils.")
